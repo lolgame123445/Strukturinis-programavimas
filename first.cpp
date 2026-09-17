3c2ae34bf1b17644c0315444699818eefa184fb7
NotHynge <kulka752@gmail.com>
2026-09-17 10:53:05 +0300

    update code

--- first.cpp
+++ first.cpp
@@ -4,13 +4,26 @@
 #include "first.h"
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    string first = "hello";
-
+string vard, pav,spp;
+int grup, kurs, age;
+cout << "koks yra jusu vardas?";
+cin >> vard;
+cout << "kokia yra jusu pavarde?";
+cin >> pav;
+cout << "koks yra jusu amzius?";
+cin >> age;
+cout << "koks yra jusu grupe?";
+cin >> grup;
+cout << "koks yra jusu kursas?";
+cin >> kurs;
+cout << "kokia jusu yra studiju pavadinimas?";
+cin >> spp;
+cout << "vardas:"<< " " << vard << " " << "pavarde:"<< " " << pav << " " << "amzius:" << " " << age << " "<<"grupe:"<<" "<< grup <<" " << "kursas:"<< " " << kurs << " " << "studiju pavadinimas:"<<spp;
 
     return 0;
 }
